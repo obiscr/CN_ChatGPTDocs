b836bcc3ab177f52281cfa8f7b54280e4950c24e
小助手 <helper@obiscr.com>
2023-03-19 09:53:51 +0800

    Deployed 66b5782 with MkDocs version: 1.4.2

--- js/baidu.js
+++ js/baidu.js
@@ -1,9 +1,7 @@
-<script>
-  var _hmt = _hmt || [];
-  (function() {
+var _hmt = _hmt || [];
+(function() {
   var hm = document.createElement("script");
   hm.src = "https://hm.baidu.com/hm.js?8d1d5a5473296de3921f31567fa71e5f";
   var s = document.getElementsByTagName("script")[0];
   s.parentNode.insertBefore(hm, s);
 })();
-</script>
